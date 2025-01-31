40f6fdf3bec9f9180016bf5be6a3b29bdb8a854f
AlProsenak <al.prosenak@gmail.com>
2025-01-31 18:07:03 +0100

    Add minor comment

--- src/main/java/dev/professional_fullstack_developer/tutorial/adapter/repository/UserJpaRepository.java
+++ src/main/java/dev/professional_fullstack_developer/tutorial/adapter/repository/UserJpaRepository.java
@@ -4,6 +4,8 @@ import dev.professional_fullstack_developer.tutorial.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // Semantic annotation is not required for interfaces extended by JPARepository, since by default it auto-instantiates the bean.
+// Semantic annotation is not required for interfaces extended by JPARepository,
+// since by default it auto-instantiates the bean, via @EnableJpaRepositories declared in JpaRepositoriesRegistrar.
+@Repository
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository {
 }
