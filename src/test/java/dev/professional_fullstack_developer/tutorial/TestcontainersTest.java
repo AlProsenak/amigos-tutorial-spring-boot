4770a0f8b2e318e3c3ced2a00642a770f14209da
AlProsenak <al.prosenak@gmail.com>
2025-02-03 15:30:33 +0100

    Add Flyway migration test

--- src/test/java/dev/professional_fullstack_developer/tutorial/TestcontainersTest.java
+++ src/test/java/dev/professional_fullstack_developer/tutorial/TestcontainersTest.java
@@ -1,5 +1,9 @@
 package dev.professional_fullstack_developer.tutorial;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -16,13 +20,35 @@ public class TestcontainersTest {
             .withUsername("postgres")
             .withPassword("postgres");
 
+    private static Flyway flyway;
+    private static MigrateResult migrationResult;
+
+    @BeforeAll
+    static void init() {
+        flyway = Flyway.configure()
+                .dataSource(container.getJdbcUrl(), container.getUsername(), container.getPassword())
+                .load();
+
+        migrationResult = flyway.migrate();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        container.close();
+    }
+
     @Test
-    void canStartPostgresDatabase() {
+    void canInitializePostgresDatabase() {
         assertThat(container.isCreated()).isTrue();
         assertThat(container.isRunning()).isTrue();
         // TODO: Fix error:
         //  java.lang.RuntimeException: This container's image does not have a healthcheck declared, so health cannot be determined. Either amend the image or use another approach to determine whether containers are healthy.
 //        assertThat(container.isHealthy()).isTrue();
+    }
+
+    @Test
+    void canApplyFlywayMigrations() {
+        assertThat(migrationResult.success).isTrue();
     }
 
 }
