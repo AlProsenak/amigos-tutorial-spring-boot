7b134a7a61bcf87f7d54bbc07596eb41dc843f8c
AlProsenak <al.prosenak@gmail.com>
2025-02-03 15:49:51 +0100

    Add dynamic propery registry to test containers

--- src/test/java/dev/professional_fullstack_developer/tutorial/TestcontainersTest.java
+++ src/test/java/dev/professional_fullstack_developer/tutorial/TestcontainersTest.java
@@ -5,6 +5,9 @@ import org.flywaydb.core.api.output.MigrateResult;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,16 +15,27 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
+// Spins up whole application context, therefore it should not be used for unit tests or simple layer/component tests, where context can be self-configured.
+//@SpringBootTest
 public class TestcontainersTest {
 
     @Container
     private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:alpine")
             .withDatabaseName("postgres")
             .withUsername("postgres")
-            .withPassword("postgres");
+            .withPassword("postgres")
+            .withExposedPorts(5432);
 
     private static Flyway flyway;
     private static MigrateResult migrationResult;
+
+    // Programmatically registers configuration properties from container, for other dependent beans to use them.
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
 
     @BeforeAll
     static void init() {
@@ -34,6 +48,7 @@ public class TestcontainersTest {
 
     @AfterAll
     static void cleanup() {
+        // Container technically already auto closes after finished tests.
         container.close();
     }
 
